44a6bb787a9f1de86502736373366462f26d0d40
nikita <professornikson@gmail.com>
2025-02-08 13:20:40 +0300

    Modified network access parametrs

--- app/src/main/java/com/example/takeanumbrella/api/ApiConfiguration.java
+++ app/src/main/java/com/example/takeanumbrella/api/ApiConfiguration.java
@@ -1,5 +1,5 @@
 package com.example.takeanumbrella.api;
 
 public interface ApiConfiguration {
-    final String BASE_URL = "http://192.168.45.35:8080/api/v1/";
+    final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
 }
