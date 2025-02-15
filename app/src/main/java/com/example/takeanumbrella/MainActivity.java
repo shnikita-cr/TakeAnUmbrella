74ab072209e655d29409fe2e605b04c5f17bb7f1
nikita <professornikson@gmail.com>
2025-02-15 13:53:19 +0300

    Added login view to bottom menu

--- app/src/main/java/com/example/takeanumbrella/MainActivity.java
+++ app/src/main/java/com/example/takeanumbrella/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.takeanumbrella;
 
 import android.os.Bundle;
 
-import com.example.takeanumbrella.test.UserTest;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,6 +9,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.takeanumbrella.databinding.ActivityMainBinding;
+import com.example.takeanumbrella.test.UserTest;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_umbrellas, R.id.navigation_profile)
+                R.id.navigation_home, R.id.navigation_umbrellas, R.id.navigation_profile, R.id.navigation_login)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
