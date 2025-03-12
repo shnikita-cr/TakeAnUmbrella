0c2f5dc751d4aa8c35986ce2829b662490ce3224
nikita <professornikson@gmail.com>
2025-03-13 00:49:31 +0300

    update retrofit client: timeouts (1 second)

--- app/src/main/java/com/example/takeanumbrella/api/RetrofitClient.java
+++ app/src/main/java/com/example/takeanumbrella/api/RetrofitClient.java
@@ -2,6 +2,9 @@ package com.example.takeanumbrella.api;
 
 import static com.example.takeanumbrella.api.ApiConfiguration.BASE_URL;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,9 +15,16 @@ public class RetrofitClient {
     }
 
     public static Retrofit getInstance() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(1, TimeUnit.SECONDS)
+                .writeTimeout(1, TimeUnit.SECONDS)
+                .readTimeout(1, TimeUnit.SECONDS)
+                .build();
+
         if (instance == null)
             instance = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         return instance;
